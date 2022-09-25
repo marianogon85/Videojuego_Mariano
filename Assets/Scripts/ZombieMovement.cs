402eeea730b41d41115f5ce009a5494d5a733d7f
marianogon85 <mariano.gonzalez@udgvirtual.udg.mx>
2022-09-25 17:51:58 -0500

    3.2. Los prefabs

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class ZombieMovement : MonoBehaviour
 {
+    public GameObject BulletPrefab;
     public float Speed;
     public float JumpForce;
 
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     private float Horizontal;
+    private bool Grounded;
+    private float LastShoot;    
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,10 +26,29 @@ public class ZombieMovement : MonoBehaviour
     {
        Horizontal = Input.GetAxisRaw("Horizontal"); 
 
-       if (Input.GetKeyDown(KeyCode.W))
+       if (Horizontal < 0.0f) transform.localScale = new Vector3(-0.08182839f, 0.06251369f, 1.392445f);
+       else if (Horizontal > 0.0f) transform.localScale = new Vector3(0.08182839f, 0.06251369f, 1.392445f);
+
+       Animator.SetBool("attacking", Horizontal != 0.0f);
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.19f, Color.red);
+       if(Physics2D.Raycast(transform.position,
+                            Vector3.down,
+                            0.19f))
+       {
+            Grounded =true;
+       }
+        else Grounded =false;
+
+       if (Input.GetKeyDown(KeyCode.W) && Grounded)
        {
             Jump();
-    
+       } 
+
+       if (Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+       {
+        Shoot();
+        LastShoot = Time.time;
        }
     }
 
@@ -33,7 +57,16 @@ public class ZombieMovement : MonoBehaviour
         Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
 
-    private void FixedUpdate()
+    private void Shoot()
+    {
+        Vector3 direction;
+        if (transform.localScale.x == 0.08182839f) direction =Vector2.right;
+        else direction = Vector2.left;
+
+        GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.08182839f, Quaternion.identity);
+        bullet.GetComponent<BulletScript>().SetDirection(direction);
+    }
+    private void FixedUpdate()  
     {
         Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
 
